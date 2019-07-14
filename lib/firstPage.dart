cadf9d41a3e707f7d7cc9463c2401585987d0c4d
Lorenzo Busolini - Bulsoft <busolinilorenzo@gmail.com>
2019-07-14 12:51:05 +0300

    modified the way to show background in body: Stack

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -3,20 +3,22 @@ import 'package:flutter/material.dart';
 class  NewPage extends StatelessWidget {
   final String title;
   
-  NewPage(this.title);
+  NewPage(this.title); 
   @override
   Widget build(BuildContext context) {
-  var sizeWidth = MediaQuery.of(context).size.width;
-  var sizeHeight = MediaQuery.of(context).size.height;
+  //var sizeWidth = MediaQuery.of(context).size.width;
+  //var sizeHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
       body: Stack(
+            fit: StackFit.expand,
             children: <Widget>[
-            Center(
-              child: Image.asset('assets/sedieCurvate.jpg'),
             
+              Image.asset('assets/sedieCurvate.jpg',
+              fit: BoxFit.cover,),
+              
            /*  Center(
             child: Image.asset(
               'assets/sedieCurvate.jpg',
@@ -28,7 +30,7 @@ class  NewPage extends StatelessWidget {
             //Image.asset('assets/giuditta_srl.jpg', width: 200.0, height: 120.0,),
             ), */
             
-            ),
+        
             Center(
             child: Text("HERE We Are",
                 style: TextStyle(
